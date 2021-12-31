2742d8b58b53068ba267aca1e1056ca47aa6b578
Shadae96 <84427990+Shadae96@users.noreply.github.com>
2021-12-31 18:35:30 -0500

    worked on HTML and JS

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+var ApiKey = 'e3e7a910d69d02a717871b60c895d7d9'
+
+
+https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=ad048d558097a59263d3151672a1c339
+
+
+
+
+
+// What we're building
+// 1.) Get weather Api
+// 2.) Input box that accepts city name
+// 3.) Use city name to pull Weather conditions 
+        // a.) date b.) time c.) 5-day forcast d.) the temperature e.) the humidity f.) UV index
+    // 4.) Icon that indicates if weather is favorable, moderate or severe
+    // 5.) create a five day forecast
+    // 6.)save results to local storage and able to click on it again to show results
+
+
+    
