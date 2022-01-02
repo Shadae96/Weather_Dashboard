6ce2099c9045b7caec6a6f522f965df77c22baa0
Shadae96 <84427990+Shadae96@users.noreply.github.com>
2022-01-02 17:53:14 -0500

    made first API Call

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,56 @@
-var ApiKey = 'e3e7a910d69d02a717871b60c895d7d9'
+var apiKey = "e3e7a910d69d02a717871b60c895d7d9";
+const searchButton = $(".search-button");
+var searchInput = $(".searchInput").val()
+
+// https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=e3e7a910d69d02a717871b60c895d7d9
+
+for (var i=0; i < localStorage.length; i++) {
+    var city = localStorage.getItem(i);
+
+    var cityName = $(".list-group").addClass("list-group-item");
+
+    cityName.append("<li>" + city + "</li>")
+}
+
+// Store to localStorage
+
+var keyCount = 0;
+
+searchButton.click(function(){
+    var searchInput = $(".searchInput").val();
+    var urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
+    var urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
+    console.log(searchInput);
+
+if (searchInput == ""){console.log(searchInput)}
+
+else { $.ajax({
+    url: urlCurrent,
+    method: "Get"})
+
+    .then(function (response) {console.log(response)
+
+    }) }
+
+});
 
 
-https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=ad048d558097a59263d3151672a1c339
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -15,6 +64,3 @@ https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hou
     // 4.) Icon that indicates if weather is favorable, moderate or severe
     // 5.) create a five day forecast
     // 6.)save results to local storage and able to click on it again to show results
-
-
-    
